708b2a77411f61d649adb99f68e112bbee0ca5dd
noorh96 <noor.hammad96@hotmail.ca>
2022-06-20 15:58:09 -0400

    implemented pub sub with single message

--- backend.js
+++ backend.js
@@ -1,7 +1,12 @@
 /* Importing Libraries */
 var express = require("express");
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
+var request = require("request");
 
+const jwt = require("jsonwebtoken");
+
+const cors = require("cors");
+const { json } = require("body-parser");
 
 /* Express Step 1: Creating an express application */
 var app = express();
@@ -9,37 +14,90 @@ var app = express();
 // set port for express app
 var port = 3000;
 
+var clientId = "jri29ztrostnximn6n29nnypngtdm6";
+
+/* The encoded key version and the decoded  version of the shared secret*/
+const key = "2Jrgbi6BRo56wJVSZJ3wDr3mveeaNe1uscDNRB4IlEE=";
+secret = Buffer.from(key, "base64");
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:8080",
+  })
+);
+
 /* Express Step 2: Start Server */
 app.listen(port, () => {
- console.log("Server listening on port (((((((((((((((()))))))))))))))) " + port);
+  console.log("Server listening on port " + port);
 });
 
 // Express Step 3: Use body-parser library to help parse incoming request bodies
 app.use(bodyParser.json());
 
-/* The encoded key version and the decoded  version of the shared secret*/
-const key ="2qh40Q1oWPqOPdIJYWEeppGPysCiDKZR42jMkjUJ8S8";
-const secret = Buffer.from(key, 'base64');
-
 //The start of the header string coming from the onAuthorized callback in the client
-const bearerPrefix = 'Bearer ';
+const bearerPrefix = "Bearer ";
 
 /** Function that takes the header from the post request and verifies that the JWT shares the same secret as the extension*/
 function verifyAndDecode(header) {
   if (header.startsWith(bearerPrefix)) {
     try {
-      const token = header.substring(bearerPrefix.length);
-      return jsonwebtoken.verify(token, secret, { algorithms: ['HS256'] });
-    }
-    catch (e) {
-      return console.log('Invalid JWT');
+      let [type, auth] = header.split(" ");
+      return jwt.verify(auth, secret, { algorithms: ["HS256"] });
+    } catch (e) {
+      return console.log("Invalid JWT" + e);
     }
   }
 }
 
+// Create and return a JWT for use by this service.
+function makeAndSignServerToken(channelId) {
+  const payload = {
+    exp: Math.floor(Date.now() / 1000) + 30,
+    channel_id: channelId,
+    user_id: "Noor_hammad", // extension owner ID for the call to Twitch PubSub
+    role: "external",
+    pubsub_perms: {
+      send: ["*"],
+    },
+  };
+  return jwt.sign(payload, secret, { algorithm: "HS256" });
+}
+
+function broadcastMessage(value,channelId) {
+  // Set the HTTP headers required by the Twitch API.
+  const headers = {
+    "Client-Id": clientId,
+    "Content-Type": "application/json",
+    "Authorization": bearerPrefix + makeAndSignServerToken(channelId),
+  };
+
+  // Create the POST body for the Twitch API request.
+  const body = JSON.stringify({
+    message: value,
+    broadcaster_id: channelId,
+    target: ["broadcast"]
+  });
+  // Send the broadcast request to the Twitch API.
+  request(
+    `https://api.twitch.tv/helix/extensions/pubsub`,
+    {
+      method: "POST",
+      headers,
+      body
+    },
+    (err, res) => {
+      if (err) {
+        console.log("ERROR WITH API POST" + err);
+      } else {
+        console.log(res.statusCode);
+      }
+    }
+  );
+}
+
 // POST: verify the auth token and header coming from client
 app.post("/auth", (req, res) => {
-     const payload = verifyAndDecode(req.headers.authorization);
-     const { channel_id: channelId, opaque_user_id: opaqueUserId } = payload;
-     console.log("Hello " + channelId);
-    });
+  const payload = verifyAndDecode(req.headers.authorization);
+  var { channel_id: channelId, opaque_user_id: opaqueUserId } = payload;
+  broadcastMessage("ping",payload.channel_id);
+});
--- video_overlay.js
+++ video_overlay.js
@@ -3,25 +3,36 @@ var token, userId;
 // so we don't have to write this out everytime 
 const twitch = window.Twitch.ext;
 
+let isAuthed = false;
 // callback called when context of an extension is fired 
 twitch.onContext((context) => {
 //   console.log(context);
 });
 
 
+twitch.listen('broadcast', (target, contentType, message) => {
+        if (isAuthed) {
+            console.log(message);
+        } else {
+            //ignore as not authed
+        }
+    }
+);
 // onAuthorized callback called each time JWT is fired
 twitch.onAuthorized((auth) => {
   // save our credentials
   token = auth.token;  
-  userId = auth.userId; 
+  userId = auth.userId;
+  isAuthed = true; 
+
   
   //ajax call that passes the JWT to the EBS along with authorized token and userId
   $.ajax({
       type: 'POST',
       url: location.protocol + '//localhost:3000/auth',
-      data: JSON.stringify({authToken:token, userId: userId}),
+      data: JSON.stringify({authToken:token, userId: userId, channelId: auth.channelId}),
       contentType: 'application/json',
-      headers: { "Authorization": 'Bearer ' + token },
+      headers: { authorization: 'Bearer ' + window.Twitch.ext.viewer.sessionToken },
   });
-
+  
 });
