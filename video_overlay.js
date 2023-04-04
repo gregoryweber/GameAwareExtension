779ed5245b0444aec99ce0f463e9fcf37dfe3845
noorh96 <noor.hammad96@hotmail.ca>
2023-04-04 13:11:13 -0400

    initial tower defense set up

--- video_overlay.js
+++ video_overlay.js
@@ -96,7 +96,7 @@ function syncBuffer(){
     for( let i = 0; i < initialBuffer.length-1; i++){
         // if(actualTime - initialBuffer[i].game_time < ((1/key_rate) * 1000)){
         if(actualTime - initialBuffer[i+1].game_time < 0){
-            actualKeyPointer = i;
+            actualKeyPointer = i-1;
             break;
             // console.log("sync point found");
         }
@@ -122,16 +122,45 @@ function syncBuffer(){
 
     initialBuffer.splice(0, actualKeyPointer);
     tweenIndex = actualTweenPointer;
-    keyFrameIndex = 1;
-    lastKeyTime = Date.now() - (actualTime - initialBuffer[keyFrameIndex-1].game_time);
-    lastTweenTime = Date.now() - (actualTime - initialBuffer[keyFrameIndex-1].tweens[tweenIndex].game_time);
-    timeToNextKey = initialBuffer[keyFrameIndex+1].game_time - initialBuffer[keyFrameIndex].game_time; 
-    if(initialBuffer[keyFrameIndex].tweens[tweenIndex+1]!=null){
-        timeToNextTween = initialBuffer[keyFrameIndex].tweens[tweenIndex+1].game_time - initialBuffer[keyFrameIndex].tweens[tweenIndex].game_time - tweenOffset;
+    keyFrameIndex = 0;
+    if(initialBuffer.length == 0){
+      console.log("NO FRAMES");
+    }
+    else if(initialBuffer.length == 1){
+      lastKeyTime = Date.now() - (actualTime - initialBuffer[0].game_time);
+      lastTweenTime = Date.now() - (actualTime - initialBuffer[0].tweens[tweenIndex].game_time);
+      timeToNextKey = 1/key_rate * 1000;
+      if(initialBuffer[0].tweens[tweenIndex+1]!=null){
+          timeToNextTween = initialBuffer[0].tweens[tweenIndex+1].game_time - initialBuffer[0].tweens[tweenIndex].game_time - tweenOffset;
+      }
+      else{
+          //todo use the key frame rate, cast to int
+          timeToNextTween = 1000 * 1000;
+      }
+    }
+    else if(initialBuffer.length == 2){
+      lastKeyTime = Date.now() - (actualTime - initialBuffer[0].game_time);
+      lastTweenTime = Date.now() - (actualTime - initialBuffer[0].tweens[tweenIndex].game_time);
+      timeToNextKey = initialBuffer[1].game_time - initialBuffer[0].game_time;
+      if(initialBuffer[0].tweens[tweenIndex+1]!=null){
+          timeToNextTween = initialBuffer[0].tweens[tweenIndex+1].game_time - initialBuffer[0].tweens[tweenIndex].game_time - tweenOffset;
+      }
+      else{
+          //todo use the key frame rate, cast to int
+          timeToNextTween = 1000 * 1000;
+      }
     }
     else{
-        //todo use the key frame rate, cast to int
-        timeToNextTween = 1000 * 1000;
+      lastKeyTime = Date.now() - (actualTime - initialBuffer[keyFrameIndex].game_time);
+      lastTweenTime = Date.now() - (actualTime - initialBuffer[keyFrameIndex].tweens[tweenIndex].game_time);
+      timeToNextKey = initialBuffer[keyFrameIndex+1].game_time - initialBuffer[keyFrameIndex].game_time; 
+      if(initialBuffer[keyFrameIndex].tweens[tweenIndex+1]!=null){
+          timeToNextTween = initialBuffer[keyFrameIndex].tweens[tweenIndex+1].game_time - initialBuffer[keyFrameIndex].tweens[tweenIndex].game_time - tweenOffset;
+      }
+      else{
+          //todo use the key frame rate, cast to int
+          timeToNextTween = 1000 * 1000;
+      }
     }
     updateWorldModelWithKey(initialBuffer[keyFrameIndex+1]);
     updateWorldModelWithTween(initialBuffer[keyFrameIndex].tweens[tweenIndex+1]);
@@ -215,7 +244,7 @@ function updateDebugOverlayRects(){
             svgRect.setAttribute("x", xOffset.toString()+"%");
             svgRect.setAttribute("y", yOffset.toString()+"%");
             svgRect.setAttribute("fill", "none");
-            // svgRect.setAttribute("stroke", "red");
+            svgRect.setAttribute("stroke", "blue");
             svgRect.setAttribute("stroke-width", "2");
             svgRect.setAttribute("position", "absolute");
             svgRect.style.pointerEvents = "all"; // prevent stroke from triggering mouse events    
@@ -291,8 +320,8 @@ function updateDebugOverlayRects(){
     //First thing to do: if an item is in the svgElements list and is no longer in the worldModel. remove it
     Object.entries(svgDebugElements).forEach(([key, value]) => {
         if (!(key in worldModel["key"])) {
-            if (parentSvg.contains(value)) {
-                parentSvg.removeChild(value);
+            if (parentSvgDebug.contains(value)) {
+                parentSvgDebug.removeChild(value);
             }
             delete svgDebugElements[key];
         }
@@ -487,7 +516,9 @@ function updateWorldModelWithKey(keyFrame){
 function updateWorldModelWithTween(tweenFrame){
     for(let item in tweenFrame){
         for(let attribute in tweenFrame[item]){
+          if(worldModel["key"][item]){
             worldModel["key"][item][attribute] = JSON.parse(JSON.stringify(tweenFrame[item][attribute]));
+          }
         }
     }
     if(tweenFrame){
